0591fe8b7d223587a98c652d91a9c29c4afdd0a4
Rami Elwan <ramielwan48@gmail.com>
2024-08-20 09:35:07 +0200

    feat: reproduce

---
+++ index.js
@@ -0,0 +1,40 @@
+const i18next = require("i18next");
+const HttpBackend = require("i18next-http-backend");
+
+let loadPath = "https://jsonplaceholder.typicode.com/osts/1";
+
+i18next
+  .use(HttpBackend)
+  .init({
+    backend: {
+      loadPath: loadPath,
+      customHeaders: {
+        "Cache-Control": "no-cache",
+        Pragma: "no-cache",
+        Expires: "0",
+      },
+      parse(data, languages, namespaces) {
+        console.log("languages", languages);
+        console.log("namespaces", namespaces);
+        return JSON.parse(data);
+      },
+    },
+    lng: "en",
+    fallbackLng: "en",
+    defaultNS: "translation",
+    ns: ["translation"],
+    debug: true,
+  })
+  .then(() => {
+    console.log(i18next.t("userId"));
+
+    loadPath = "https://jsonplaceholder.typicode.com/posts/1";
+
+    setTimeout(() => {
+      console.log("will reload");
+      i18next.reloadResources().then(() => {
+        console.log(i18next.t("userId"));
+        console.log("reloaded");
+      });
+    }, 1000);
+  });
